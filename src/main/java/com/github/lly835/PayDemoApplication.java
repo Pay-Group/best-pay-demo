7de6a5a0ac6fb5f4ede862357a131706271ae108
lly835 <lly835@163.com>
2020-05-23 12:35:27 +0800

    把demo从best-pay移除，作为单独项目

---
+++ src/main/java/com/github/lly835/PayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.lly835;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class PayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayDemoApplication.class, args);
+	}
+}
